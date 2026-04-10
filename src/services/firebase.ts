bd3bd35e93e236eac934e511a1356356e2f2140c
Fabricio Oliveira <fabricioliveira021@gmail.com>
2026-04-09 21:08:43 -0300

    fix(build): expose firebase config for eas build

---
+++ src/services/firebase.ts
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeFirestore, persistentLocalCache } from "firebase/firestore";
+import { getFunctions } from "firebase/functions";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgACE3-XgVdhUUzPENo8C2crF9eAIvqX0",
+  authDomain: "cftvk-edcff.firebaseapp.com",
+  projectId: "cftvk-edcff",
+  storageBucket: "cftvk-edcff.firebasestorage.app",
+  messagingSenderId: "685906532982",
+  appId: "1:685906532982:web:d54964b904484a081e1d57"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache(),
+});
+export const functions = getFunctions(app);
+
+console.log('Firebase initialized');
